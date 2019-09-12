80ec4321fba374b3ad50123d52e05e159c7ec069
gaoss-huhu <1348206093@qq.com>
2019-09-12 16:59:07 +0800

    hu

---
+++ src/login/index.js
@@ -0,0 +1,17 @@
+// React.createClass({})
+import {Component} from 'react'
+class Huhu extends Component{
+    constructor (props){
+        super()
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>胡胡宝宝</div>
+        )
+    }
+
+}
+export default Huhu
