a9113962a618b72e954db71c8bca10dd76e5ef89
Your Name <you@example.com>
2019-09-20 22:05:29 +0800

    dad

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,19 +1,19 @@
 import  axios from 'axios'
 import Store from '../store/store'
 import ActionCreator from '../store/actioncreator'
-axios.interceptors.request.use(function (config) {
-  let {method,data} = config
-  let token  = localStorage.getItem('token')
-  if(method === 'get'){
-    config.url+=`&token=${token}`
-  }
-  if(method === 'post'){
-    data.token=`${token}`
-  }
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
+// axios.interceptors.request.use(function (config) {
+//   let {method,data} = config
+//   let token  = localStorage.getItem('token')
+//   if(method === 'get'){
+//     config.url+=`&token=${token}`
+//   }
+//   if(method === 'post'){
+//     data.token=`${token}`
+//   }
+//   return config;
+// }, function (error) {
+//   return Promise.reject(error);
+// });
 
 axios.interceptors.response.use(function (response) {//请求拦截器
   if(response.status === 200){
