4d887a8f2cd535f4c5161cd30937593acf231c0a
xiaojiao <ita1.wxxcx@luftc2c.com>
2019-12-18 22:50:18 +0800

    修改

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import React,{Component} from 'react'
 import {HashRouter,Route,Switch,Redirect} from 'react-router-dom'
 import App from './App'
-<<<<<<< HEAD
-import loadable from  'utils/loadingComponent'
-const  Admin = loadable(()=>import('pages/admin'))  
-const  FoodList = loadable(()=>import('pages/food')) 
-const  FoodAdd = loadable(()=>import('pages/foodadd')) 
-const  OrderList = loadable(()=>import('pages/order/orderList')) 
-=======
+
+// import loadable from  'utils/loadingComponent'
+// const  Admin = loadable(()=>import('pages/admin'))  
+// const  FoodList = loadable(()=>import('pages/food')) 
+// const  FoodAdd = loadable(()=>import('pages/foodadd')) 
+// const  OrderList = loadable(()=>import('pages/order/orderList')) 
+
 import Login from 'pages/login'
 import Admin from 'pages/admin'
 import Adensen from 'pages/market'
@@ -15,7 +15,6 @@ import FoodList from 'pages/food'
 import FoodAdd from 'pages/foodadd'
 import OrderList from 'pages/order/orderList'
 import Home from 'pages/home'
->>>>>>> f8d8f5f52122576c7d8990fa3852efeca1dad3ae
 class RootRouter extends Component{
     render(){
         return(
